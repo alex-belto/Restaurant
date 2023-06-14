72e2297f00fe7d80097c2759d5a49cb6d474d250
Alex <tyazhkorob.aleks@icloud.com>
2023-06-14 20:52:24 +0300

    made client entity

---
+++ src/Entity/Client.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClientRepository::class)]
+class Client
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $money = null;
+
+    #[ORM\ManyToMany(targetEntity: MenuItem::class)]
+    private Collection $menuItems;
+
+    public function __construct()
+    {
+        $this->menuItems = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMoney(): ?float
+    {
+        return $this->money;
+    }
+
+    public function setMoney(float $money): static
+    {
+        $this->money = $money;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, MenuItem>
+     */
+    public function getMenuItems(): Collection
+    {
+        return $this->menuItems;
+    }
+
+    public function addMenuItem(MenuItem $menuItem): static
+    {
+        if (!$this->menuItems->contains($menuItem)) {
+            $this->menuItems->add($menuItem);
+        }
+
+        return $this;
+    }
+
+    public function removeMenuItem(MenuItem $menuItem): static
+    {
+        $this->menuItems->removeElement($menuItem);
+
+        return $this;
+    }
+}

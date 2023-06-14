6ba98a6a25911f835afaf56ac0d33547fac2b289
Alex <tyazhkorob.aleks@icloud.com>
2023-06-14 14:01:25 +0300

    made Menu Fixtures

--- Factory/MenuItemFactory.php
+++ Factory/MenuItemFactory.php
@@ -4,17 +4,33 @@ namespace App\Factory;
 
 use App\Entity\Dish;
 use App\Entity\Drink;
-use JetBrains\PhpStorm\Pure;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MenuItemFactory
 {
+    /** @var EntityManagerInterface */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function createDish(string $name, float $price, string $time): Dish
     {
-        return new Dish($name, $price, $time);
+        $dish = new Dish($name, $price, $time);
+        $this->em->persist($dish);
+        $this->em->flush();
+
+        return $dish;
     }
 
     public function createDrink(string $name, float $price, string $time): Drink
     {
-        return new Drink($name, $price, $time);
+        $drink = new Drink($name, $price, $time);
+        $this->em->persist($drink);
+        $this->em->flush();
+
+        return $drink;
     }
 }

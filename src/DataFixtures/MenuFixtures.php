c83440ebe104f839bae1fb42f7fa0fedc6625c5b
Alex <tyazhkorob.aleks@icloud.com>
2023-06-18 23:43:10 +0300

    update menu fixtures

--- src/DataFixtures/MenuFixtures.php
+++ src/DataFixtures/MenuFixtures.php
@@ -2,40 +2,41 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\MenuItemFactory;
+use App\Entity\MenuItem;
+use App\Services\Menu\CreateMenuItem;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class MenuFixtures extends Fixture
 {
-    private MenuItemFactory $menuFactory;
+    private  CreateMenuItem $createMenuItem;
 
-    public function __construct(MenuItemFactory $menuFactory)
+    public function __construct(CreateMenuItem $createMenuItem)
     {
-        $this->menuFactory = $menuFactory;
+        $this->createMenuItem = $createMenuItem;
     }
 
     public function load(ObjectManager $manager)
     {
-        $this->menuFactory->createDish('Pizza Margarita', 7.5, '0.5h');
-        $this->menuFactory->createDish('Pizza Diablo', 8.7, '0.5h');
-        $this->menuFactory->createDish('Pizza Hawaii', 8.0, '0.5h');
-        $this->menuFactory->createDish('Pizza Peperoni', 8.2, '0.5h');
-        $this->menuFactory->createDish('Spaghetti Bolognese', 6.0, '0.5h');
-        $this->menuFactory->createDish('Spaghetti Tonne', 6.0, '0.5h');
-        $this->menuFactory->createDish('Chicken Soup', 4.3, '0.5h');
-        $this->menuFactory->createDish('Chicken Broth', 3.7, '0.5h');
-        $this->menuFactory->createDish('Borsch', 8.0, '0.5h');
-        $this->menuFactory->createDish('Spinach puree', 7.5, '0.5h');
-        $this->menuFactory->createDish('French fries', 4.0, '0.3h');
-        $this->menuFactory->createDish('Beef Steak', 10.5, '0.5h');
-        $this->menuFactory->createDish('Donner', 6.5, '0.2h');
-        $this->menuFactory->createDish('Pancakes', 5.0, '0.5h');
-        $this->menuFactory->createDish('Apple Pie', 6.5, '0.1h');
+        $this->createMenuItem->createMenuItem('Pizza Margarita', 7.5, '0.5h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Pizza Diablo', 8.7, '0.5h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Pizza Hawaii', 8.0, '0.5h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Pizza Peperoni', 8.2, '0.5h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Spaghetti Bolognese', 6.0, '0.5h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Spaghetti Tonne', 6.0, '0.5h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Chicken Soup', 4.3, '0.5h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Chicken Broth', 3.7, '0.5h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Borsch', 8.0, '0.5h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Spinach puree', 7.5, '0.5h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('French fries', 4.0, '0.3h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Beef Steak', 10.5, '0.5h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Donner', 6.5, '0.2h',MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Pancakes', 5.0, '0.5h', MenuItem::DISH);
+        $this->createMenuItem->createMenuItem('Apple Pie', 6.5, '0.1h', MenuItem::DISH);
 
-        $this->menuFactory->createDrink('Cola', '1.5', '0.1h');
-        $this->menuFactory->createDrink('Spring Water', '1.0', '0.1h');
-        $this->menuFactory->createDrink('Coffee', '1.0', '0.1h');
-        $this->menuFactory->createDrink('Tea', '1.0', '0.1h');
+        $this->createMenuItem->createMenuItem('Cola', '1.5', '0.1h', MenuItem::DRINK);
+        $this->createMenuItem->createMenuItem('Spring Water', '1.0', '0.1h', MenuItem::DRINK);
+        $this->createMenuItem->createMenuItem('Coffee', '1.0', '0.1h', MenuItem::DRINK);
+        $this->createMenuItem->createMenuItem('Tea', '1.0', '0.1h', MenuItem::DRINK);
     }
 }
---
+++ src/Services/Menu/CreateMenuItem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services\Menu;
+
+use App\Entity\MenuItem;
+use Doctrine\ORM\EntityManagerInterface;
+
+class CreateMenuItem
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em) {
+        $this->em = $em;
+    }
+    public function createMenuItem(string $name, float $price, string $time, int $type): void
+    {
+        $menuItem = new MenuItem();
+        $menuItem->setName($name);
+        $menuItem->setPrice($price);
+        $menuItem->setTime($time);
+        $menuItem->setType($type);
+
+        $this->em->persist($menuItem);
+        $this->em->flush();
+    }
+
+}

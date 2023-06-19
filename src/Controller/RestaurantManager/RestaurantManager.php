a69e32e6934905f2a6ceb7b0aee0e8dd49ed06ef
Alex <tyazhkorob.aleks@icloud.com>
2023-06-19 22:53:48 +0300

    add restaurant work logic

--- src/Controller/RestaurantManager/RestaurantManager.php
+++ src/Controller/RestaurantManager/RestaurantManager.php
@@ -2,6 +2,39 @@
 
 namespace App\Controller\RestaurantManager;
 
-class RestaurantManager
+use App\Services\Client\ClientManager;
+use App\Services\Waiter\WaiterOrderManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RestaurantManager extends AbstractController
 {
+    /**
+     * @var WaiterOrderManager
+     */
+    private $waiterOrderManager;
+
+    /**
+     * @var ClientManager
+     */
+    private $clientManager;
+
+    /**
+     * @param WaiterOrderManager $waiterOrderManager
+     * @param ClientManager $clientManager
+     */
+    public function __construct(
+        WaiterOrderManager $waiterOrderManager,
+        ClientManager $clientManager
+    ) {
+        $this->waiterOrderManager = $waiterOrderManager;
+        $this->clientManager = $clientManager;
+    }
+
+    #[Route('/restaurant/open/{days}', name: 'open_restaurant', methods: ['GET'])]
+    public function openRestaurant(int $days): void
+    {
+        $client = $this->clientManager->addClient(true);
+        $this->waiterOrderManager->processingOrder($client);
+    }
 }

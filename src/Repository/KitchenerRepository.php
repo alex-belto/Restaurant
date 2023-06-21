de802a334a212695a5f121f1c9733fecbb7fe428
Alex <tyazhkorob.aleks@icloud.com>
2023-06-21 12:56:26 +0300

    add kitchener repository

---
+++ src/Repository/KitchenerRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Kitchener;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Kitchener>
+ *
+ * @method Kitchener|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Kitchener|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Kitchener[]    findAll()
+ * @method Kitchener[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class KitchenerRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Kitchener::class);
+    }
+
+    public function save(Kitchener $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Kitchener $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Kitchener[] Returns an array of Kitchener objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('k')
+//            ->andWhere('k.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('k.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Kitchener
+//    {
+//        return $this->createQueryBuilder('k')
+//            ->andWhere('k.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}

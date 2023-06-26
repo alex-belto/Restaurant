93e1c452a8d2a204c32a81b66c970bb565f77133
Alex <tyazhkorob.aleks@icloud.com>
2023-06-26 20:50:30 +0300

    payment bug fix

--- src/Services/Payment/PayOrder.php
+++ src/Services/Payment/PayOrder.php
@@ -14,7 +14,7 @@ class PayOrder
     private $em;
 
     /**
-     * @var EntityManagerInterface $em
+     * @param EntityManagerInterface $em
      */
     public function __construct(EntityManagerInterface $em) {
         $this->em = $em;

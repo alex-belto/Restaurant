b878d25a5ec4c3fc3ff890bd2abb6d84e8c1b619
Alex <tyazhkorob.aleks@icloud.com>
2023-07-25 17:33:13 +0300

    - updated test for CardPaymentProcessorTest service    -added few new test for exceptions

--- tests/Services/CardPaymentProcessorTest.php
+++ tests/Services/CardPaymentProcessorTest.php
@@ -4,8 +4,10 @@ namespace App\Tests\Services;
 
 use App\Entity\Client;
 use App\Entity\Order;
+use App\Exception\CardValidationException;
 use App\Services\Payment\CardPaymentProcessor;
 use App\Services\Payment\Payment;
+use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
 
@@ -38,11 +40,53 @@ class CardPaymentProcessorTest extends TestCase
         $em->expects($this->once())
             ->method('beginTransaction');
 
+        $client->expects($this->once())
+            ->method('setStatus')
+            ->with(Client::ORDER_PAYED);
+
         $em->expects($this->once())
             ->method('flush');
 
         $em->expects($this->once())
             ->method('commit');
+
+        $cardPaymentProcessor->pay($client, $order);
+    }
+
+    public function testCardValidationException(): void
+    {
+        $processingPayment = $this->getMockBuilder(Payment::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $em = $this->createMock(EntityManagerInterface::class);
+
+        $cardPaymentProcessor = new CardPaymentProcessor($processingPayment, $em);
+
+        $order = $this->createMock(Order::class);
+        $client = $this->createMock(Client::class);
+
+        $client->method('isCardValid')->willReturn(false);
+        $this->expectException(CardValidationException::class);
+
+        $cardPaymentProcessor->pay($client, $order);
+    }
+
+    public function testisEnoughMoneyException(): void
+    {
+        $processingPayment = $this->getMockBuilder(Payment::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $em = $this->createMock(EntityManagerInterface::class);
+
+        $cardPaymentProcessor = new CardPaymentProcessor($processingPayment, $em);
+
+        $order = $this->createMock(Order::class);
+        $client = $this->createMock(Client::class);
+
+        $client->method('isEnoughMoney')->willReturn(false);
+        $this->expectException(Exception::class);
 
         $cardPaymentProcessor->pay($client, $order);
     }

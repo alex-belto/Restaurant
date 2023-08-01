e868d71039071b4790798f312f9d69dbdedc5045
Alex <tyazhkorob.aleks@icloud.com>
2023-08-01 15:51:46 +0300

    restaurantProvider test upd

--- tests/Unit/Services/Restaurant/RestaurantProviderTest.php
+++ tests/Unit/Services/Restaurant/RestaurantProviderTest.php
@@ -15,6 +15,8 @@ class RestaurantProviderTest extends TestCase
     private EntityManagerInterface $em;
     private RestaurantProvider $restaurantProvider;
     private string $filePath;
+    private RestaurantRepository $restaurantRepository;
+    private Restaurant $restaurant;
 
     public function setUp(): void
     {
@@ -22,55 +24,58 @@ class RestaurantProviderTest extends TestCase
         $this->em = $this->createMock(EntityManagerInterface::class);
         $this->restaurantProvider = new RestaurantProvider($this->restaurantBuilder, $this->em);
         $this->filePath = realpath(__DIR__ . '/../../../..') . $_ENV['FILE_PATH'];
+        $this->restaurantRepository = $this->createMock(RestaurantRepository::class);
+        $this->restaurant = $this->createMock(Restaurant::class);
     }
 
-//    public function testNotExistRestaurant(): void
-//    {
-//        if (file_exists($this->filePath)) {
-//            unlink($this->filePath);
-//        }
-//
-//        $restaurant = $this->createMock(Restaurant::class);
-//        $restaurantRepository = $this->createMock(RestaurantRepository::class);
-//        $this->em->method('getRepository')->willReturn($restaurantRepository);
-//
-//        $this->restaurantBuilder
-//            ->expects($this->once())
-//            ->method('buildRestaurant')
-//            ->willReturn($restaurant);
-//
-//        $this->em
-//            ->expects($this->never())
-//            ->method('find');
-//
-//        $restaurant = $this->restaurantProvider->getRestaurant(1);
-//        $this->assertInstanceOf(Restaurant::class, $restaurant);
-//    }
+    public function tearDown(): void
+    {
+        if (file_exists($this->filePath)) {
+            unlink($this->filePath);
+        }
+    }
+
+    public function testNotExistRestaurant(): void
+    {
+        if (file_exists($this->filePath)) {
+            unlink($this->filePath);
+        }
+
+        $this->em->method('getRepository')->willReturn($this->restaurantRepository);
+
+        $this->restaurantBuilder
+            ->expects($this->once())
+            ->method('buildRestaurant')
+            ->willReturn($this->restaurant);
+
+        $this->em
+            ->expects($this->never())
+            ->method('find');
+
+        $restaurant = $this->restaurantProvider->getRestaurant(1);
+        $this->assertInstanceOf(Restaurant::class, $restaurant);
+    }
 
     public function testRestaurantFileExist(): void
     {
         $restaurantId = 111;
-        if (!file_exists($this->filePath)) {
-            file_put_contents($this->filePath, $restaurantId);
-        }
-
-        $restaurantRepository = $this->createMock(RestaurantRepository::class);
-        $restaurant = $this->createMock(Restaurant::class);
+        file_put_contents($this->filePath, $restaurantId);
 
         $this->restaurantBuilder
-            ->expects($this->never());
+            ->expects($this->never())
+            ->method('buildRestaurant');
 
         $this->em
             ->expects($this->once())
             ->method('getRepository')
             ->with($this->equalTo(Restaurant::class))
-            ->willReturn($restaurantRepository);
+            ->willReturn($this->restaurantRepository);
 
-        $restaurantRepository
+        $this->restaurantRepository
             ->expects($this->once())
             ->method('find')
             ->with($this->equalTo($restaurantId))
-            ->willReturn($restaurant);
+            ->willReturn($this->restaurant);
 
         $restaurant = $this->restaurantProvider->getRestaurant();
         $this->assertInstanceOf(Restaurant::class, $restaurant);

c2d1d29284318432bcb5046019d01fd5eac1ef65
Alex <tyazhkorob.aleks@icloud.com>
2023-08-05 16:52:30 +0300

    remove service client manager to listener clientListener create ClientListenerTest

---
+++ tests/Unit/Listeners/Client/ClientListenerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Tests\Unit\Listeners\Client;
+
+use App\Entity\Client;
+use App\Entity\MenuItem;
+use App\Entity\Order;
+use App\Entity\Restaurant;
+use App\EventListener\Client\ClientListener;
+use App\Services\OrderItem\OrderItemFactory;
+use App\Services\Restaurant\RestaurantProvider;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+
+class ClientListenerTest extends TestCase
+{
+    public function testMakeOrder(): void
+    {
+        $restaurantProvider = $this->createMock(RestaurantProvider::class);
+        $orderItemFactory = $this->createMock(OrderItemFactory::class);
+        $em = $this->createMock(EntityManagerInterface::class);
+        $clientListener = new ClientListener($restaurantProvider, $orderItemFactory, $em);
+        $client = $this->createMock(Client::class);
+        $restaurant = $this->createMock(Restaurant::class);
+        $menuItem = $this->createMock(MenuItem::class);
+        $menu = new ArrayCollection([$menuItem, $menuItem, $menuItem, $menuItem]);
+
+        $restaurantProvider
+            ->expects($this->once())
+            ->method('getRestaurant')
+            ->willReturn($restaurant);
+
+        $restaurant
+            ->expects($this->once())
+            ->method('getMenuItems')
+            ->willReturn($menu);
+
+        $orderItemFactory
+            ->expects($this->exactly(3))
+            ->method('createOrderItem');
+
+        $client
+            ->expects($this->once())
+            ->method('setStatus')
+            ->with($this->equalTo(Client::ORDER_PLACED));
+
+        $client
+            ->expects($this->once())
+            ->method('setConnectedOrder');
+
+        $em
+            ->expects($this->exactly(4))
+            ->method('persist');
+
+        $em
+            ->expects($this->once())
+            ->method('flush');
+
+        $order = $clientListener->makeOrder($client);
+        $this->assertInstanceOf(Order::class, $order);
+    }
+
+}

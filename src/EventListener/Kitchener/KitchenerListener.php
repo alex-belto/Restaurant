85a4e0cfed70fa76abc209bfbdc3860dfc6ba89e
Alex <tyazhkorob.aleks@icloud.com>
2023-08-15 21:31:12 +0300

    removed KitchenerOrderProcessor.php to KitchenerListener.php

--- src/EventListener/Kitchener/KitchenerListener.php
+++ src/EventListener/Kitchener/KitchenerListener.php
@@ -2,20 +2,25 @@
 
 namespace App\EventListener\Kitchener;
 
+use App\Entity\Kitchener;
 use App\Entity\Order;
-use App\Services\Kitchener\KitchenerOrderProcessor;
+use App\Services\Staff\StaffResolver;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * Listening to the order, after we update its status to "READY_TO_KITCHEN" and proceed with the processing.
  */
 class KitchenerListener
 {
-    private KitchenerOrderProcessor $kitchenerOrderProcessor;
+    private EntityManagerInterface $em;
+    private StaffResolver $staffResolver;
 
     public function __construct(
-        KitchenerOrderProcessor $kitchenerOrderProcessor
+        EntityManagerInterface $em,
+        StaffResolver $staffResolver
     ) {
-        $this->kitchenerOrderProcessor = $kitchenerOrderProcessor;
+        $this->em = $em;
+        $this->staffResolver = $staffResolver;
     }
 
     /**
@@ -27,7 +32,11 @@ class KitchenerListener
             return;
         }
 
-        $this->kitchenerOrderProcessor->processingOrder($order);
+        /** @var Kitchener $kitchener */
+        $kitchener = $this->staffResolver->chooseStaff('kitchener');
+        $kitchener->addOrder($order);
+        $order->setStatus(Order::READY_TO_WAITER);
+        $this->em->flush();
     }
 
 }

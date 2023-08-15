78c99625ea3a4bc86540f32bd0fdc5cff103ae62
Alex <tyazhkorob.aleks@icloud.com>
2023-08-15 21:05:21 +0300

    removed unused dependency

--- src/DataFixtures/MenuFixtures.php
+++ src/DataFixtures/MenuFixtures.php
@@ -4,7 +4,6 @@ namespace App\DataFixtures;
 
 use App\Services\Menu\MenuItemCreator;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 
 /**
@@ -13,29 +12,25 @@ use Doctrine\Persistence\ObjectManager;
 class MenuFixtures extends Fixture
 {
     private  MenuItemCreator $menuItemCreator;
-    private EntityManagerInterface $em;
 
     public function __construct(
         MenuItemCreator $menuItemCreator,
-        EntityManagerInterface $em
-    )
-    {
+    ) {
         $this->menuItemCreator = $menuItemCreator;
-        $this->em = $em;
     }
 
     public function load(ObjectManager $manager)
     {
         for ($i = 1; $i <= 15; $i++) {
             $dish = $this->menuItemCreator->createDish();
-            $this->em->persist($dish);
+            $manager->persist($dish);
         }
 
         for ($j = 1; $j <= 5; $j++) {
             $drink = $this->menuItemCreator->createDrink();
-            $this->em->persist($drink);
+            $manager->persist($drink);
         }
 
-        $this->em->flush();
+        $manager->flush();
     }
 }
